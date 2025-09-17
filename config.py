75a55ab20f4f50284881a300474ccd790366a891
rohit99504 <163815811+rohit99504@users.noreply.github.com>
2025-09-17 08:31:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,17 +13,17 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+API_ID = int(getenv("API_ID", "27100684"))
+API_HASH = getenv("API_HASH", "3d9ff72941b7c8ecafbadf7d625f0108")
+BOT_TOKEN = getenv("BOT_TOKEN", "7108754701:AAFaL30XI1Prve2S-KtoPC2yM0SHZ9alfsQ")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "7616549285").split()))
+MONGO_DB = getenv("MONGO_DB", None)
+LOG_GROUP = getenv("LOG_GROUP", "-1002812924241")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002812924241"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "1000"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
-AD_API = getenv("AD_API", "")
+AD_API = getenv("AD_API", None)
 STRING = getenv("STRING", None)
 YT_COOKIES = getenv("YT_COOKIES", YTUB_COOKIES)
 DEFAULT_SESSION = getenv("DEFAUL_SESSION", None)  # added old method of invite link joining
